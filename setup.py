4b70948383ff444c75c1d4916d47caedaefdd423
Rokic <Rokic.github@gmail.com>
2016-03-23 13:47:52 +0800

    Setup Update

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='ooxx1024',
-    version='0.1.0',
+    version='0.1.1',
 	description='A high-speed Robust Torrent Download Package',
 	author='Rokic',
+	license="GPL",
 	packages=find_packages(),
 	include_package_data=True,
 	zip_safe=False,
+	classifiers=[
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+	],
     install_requires=[
         'requests',
 		'beautifulsoup4>4.1',
